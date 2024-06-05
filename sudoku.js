2d90293f50dfd2a4b6396b1d53e43b2fc91746a1
PiaLember <pia.lember@gmail.com>
2024-06-05 15:16:56 +0300

    Add the board

--- sudoku.js
+++ sudoku.js
@@ -31,11 +31,21 @@ window.onload = function() {
 }
 
 function setGame() {
+    //Digits 1-9
     for (let i = 1; i<=9; i++) {
         let number = document.createElement("div");
         number.id = i
         number.innerText = i;
         number.classList.add("number");
         document.getElementById("digits").appendChild(number);
+    }
+    //Board
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            document.getElementById("board").append(tile);
+        }
     }
 }
